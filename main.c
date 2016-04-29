bef2fa328eae06e6b39fb7456c4b41c7924e8e86
Jose Pascarella Quijada <josepas979@gmail.com>
2016-04-29 11:46:41 -0430

    gitignore y lexer token NL

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char const *argv[]) {
+
+
+
+
+
+
+
+
+
+	if (0) 
+		return(EXIT_SUCCESS);
+	else {
+		return(EXIT_FAILURE);
+	}
+}
