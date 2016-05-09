f8a6d417938b1b9b7f56168ee7bb4416687b1e11
Amin Arria <arria.amin@gmail.com>
2016-05-09 17:09:25 -0400

    Primer caso de prueba

--- SymbolTable/sttesting.c
+++ SymbolTable/sttesting.c
@@ -1,9 +1,33 @@
 #include "symbolTable.h"
 
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
 	// probando la tabla de simbolos
-	
+    Symtable *current = createTable(NULL);
+
+    insertTable(current, "a", 1, 2);
+    insertTable(current, "cola", 1, 2);
+
+    enterScope(&current);
+
+    insertTable(current, "gusano", 1, 2);
+    insertTable(current, "hashTabl", 1, 2);
+
+    enterScope(&current);
+
+    insertTable(current, "Hashy Potter", 1, 2);
+
+    exitScope(&current);
+
+    insertTable(current, "Hashy Potter", 1, 2);
+
+    exitScope(&current);
+
+    insertTable(current, "hashTabl", 1, 2);
+
+    dumpTable(current);
+
+    destroyTable(current);
+
 	return 0;
 }
