053b644d7faac33fe8fa8de38515a0d919d92a75
Jose Pascarella Quijada <josepas979@gmail.com>
2016-05-31 16:43:55 -0400

    arrglados problemas de circularidad

--- TypeTree/typeTree.c
+++ TypeTree/typeTree.c
@@ -59,3 +59,32 @@ void dumpType(Typetree *type) {
             break;
     }
 }
+
+
+ArgList* newArgList(Typetree* t) {
+
+    ArgList *newL;
+    newL = (ArgList*) memory( sizeof(ArgList) );
+
+    TypeNode *newN;
+    newN = (TypeNode*) memory( sizeof(TypeNode));
+    newN->t = t;
+    newN->next = NULL;
+
+    newL->f = newN;
+    newL->l = newN;
+
+    return newL;
+}
+
+ArgList* add(ArgList* l, Typetree* t) {
+    TypeNode *newN;
+    newN = (TypeNode*) memory( sizeof(TypeNode) );
+    newN->t = t;
+    newN->next = NULL;
+
+    l->l->next = newN;
+    l->l = newN;
+
+    return l;
+}
--- utils/utils.h
+++ utils/utils.h
@@ -1,25 +1,8 @@
-#ifndef UTILS
-#define UTILS
 #include <stdio.h>
 #include <stdlib.h>
 #include "../TypeTree/typeTree.h"
 
-typedef struct _argList ArgList;
-
-typedef struct _typeNode {
-    struct _typetree *t;
-    struct _typeNode *next;
-} TypeNode;
-
-struct _argList {
-    TypeNode *f;
-    TypeNode *l;
-};
 
 void* memory(size_t);
 
-ArgList* newArgList(struct _typetree*);
 
-ArgList* add(ArgList*, struct _typetree*);
-
-#endif
