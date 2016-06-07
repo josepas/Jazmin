0c7996a40ff784336d72a5338cd3b0c3e8a0b1a6
Jose Pascarella Quijada <josepas979@gmail.com>
2016-06-06 23:00:30 -0400

    actualizado el to-do

--- SymbolTable/symbolTable.h
+++ SymbolTable/symbolTable.h
@@ -6,12 +6,15 @@
 #include "../utils/utils.h"
 #include "../TypeTree/typeTree.h"
 
+// typedef enum {T_TYPE, T_VAR, T_FIELD, T_FUNC, T_PROC, T_POINTER, T_ARRAY, T_RECORD,
+//                 T_TYPE, T_TYPE_ERROR, T_ANYTYPE} tsKind;
+
 typedef struct _entry {
     char *string;
     int line;
     int column;
-    struct _entry *next;
     struct _typetree *type;
+    struct _entry *next;
 } Entry;
 
 typedef struct _symtable {
@@ -21,8 +24,6 @@ typedef struct _symtable {
     struct _symtable *next;
     Entry **table;
 } Symtable;
-
-
 
 Symtable* createTable(Symtable*);
 Symtable* enterScope(Symtable*);
