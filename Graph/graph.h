ffa4fcd5f3e2b5e71b8c8925949c0282e0a622a7
Jose Pascarella Quijada <josepas979@gmail.com>
2016-10-03 09:27:48 -0400

    comienzo del grafo

--- Graph/graph.h
+++ Graph/graph.h
@@ -1,12 +1,28 @@
 #ifndef DEF_GRAPH
 #define DEF_GRAPH
+#include "List/DoublyLinkedList.h"
 
 
 typedef struct _graph {
 
+    DLinkedList *code;
+
+    struct _graph next;
+    struct _graph jump;
+
 
 } Graph;
 
+void newGraph(DLinkedList*);
+
+void newGraph(DLinkedList* code) {
+    Graph* g = (Graph*)malloc(sizeof(Graph));
+
+    g->code = code;
+    g->next = NULL;
+    g->jump = NULL;
+
+}
 
 
 #endif
